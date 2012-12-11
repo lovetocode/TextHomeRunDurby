7b068c8368b043750a3833462e95a6c8cfd68655
Daniel Thornton <dshaggy28@yahoo.com>
2012-12-10 23:50:11 -0800

    first commit on my first game

---
+++ homerundurby.py
@@ -0,0 +1,27 @@
+def startScreen():
+	start = """ Hello And welcom to:
+				Home Run Durby
+				    
+	 The baseball game made by the man who cant spell
+	              
+				   Version 1.0 Alpha
+                				                   
+						
+	"""                
+	print(start)	  
+
+#startScreen()
+
+def endCredits():
+	over = '''
+	Sorry!!
+		But your games is over
+		Was not Home Run Durby Great
+		Developed by No good games 2012
+		
+	'''
+	print(over)
+#endCredits()
+
+
+					
